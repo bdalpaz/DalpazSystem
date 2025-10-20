764be14e46a4289e5044c22f7f56cc6aa036e4fa
Lice <alicedalpaz03@gmail.com>
2025-10-20 16:21:19 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hiddenElements = document.querySelectorAll('.hidden');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            }
+        });
+    }, {
+        threshold: 0.1 
+    });
+
+    hiddenElements.forEach((el) => observer.observe(el));
+});
